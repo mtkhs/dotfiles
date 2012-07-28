3a072e516a7992298e1a56ee8854a041819ae859
Motoi Takahashi <chakopu@gmail.com>
2012-07-29 04:24:03 +0900

    update

--- init.sh
+++ init.sh
@@ -9,7 +9,7 @@ git submodule update --init
 ln -s $DOTFILES/.zshrc $HOME/.zshrc
 ln -s $DOTFILES/.zshenv $HOME/.zshenv
 ln -s $DOTFILES/.zprofile $HOME/.zprofile
-ln -s $DOTFILES/.vim/ $HOME/.vim/
+ln -s $DOTFILES/.vim $HOME/.vim
 ln -s $DOTFILES/.vimrc $HOME/.vimrc
 ln -s $DOTFILES/.tmux.conf $HOME/.tmux.conf
 
