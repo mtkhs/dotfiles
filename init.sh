175393a37eca69042d5475eddf66b4a74c3b1e22
Motoi Takahashi <chakopu@gmail.com>
2012-07-30 13:59:29 +0900

    update

--- init.sh
+++ init.sh
@@ -6,12 +6,12 @@ cd $DOTFILES
 
 git submodule update --init --recursive
 
-ln -s $DOTFILES/.zshrc      $HOME/.zshrc
-ln -s $DOTFILES/.zshenv     $HOME/.zshenv
-ln -s $DOTFILES/.zprofile   $HOME/.zprofile
-ln -s $DOTFILES/.vim/       $HOME/.vim
-ln -s $DOTFILES/.vimrc      $HOME/.vimrc
-ln -s $DOTFILES/.tmux.conf  $HOME/.tmux.conf
+ln -s    $DOTFILES/.zshrc      $HOME/.zshrc
+ln -s    $DOTFILES/.zshenv     $HOME/.zshenv
+ln -s    $DOTFILES/.zprofile   $HOME/.zprofile
+ln -s -T $DOTFILES/.vim        $HOME/.vim
+ln -s    $DOTFILES/.vimrc      $HOME/.vimrc
+ln -s    $DOTFILES/.tmux.conf  $HOME/.tmux.conf
 
 vim -c ':NeoBundleInstall' -c q
 
