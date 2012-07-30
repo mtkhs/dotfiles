2ac6637d9e8cb8d90a5de073ead85c993faf95b6
Motoi Takahashi <chakopu@gmail.com>
2012-07-30 09:43:11 +0900

    update

--- init.sh
+++ init.sh
@@ -6,18 +6,15 @@ cd $DOTFILES
 
 git submodule update --init
 
-ln -s $DOTFILES/.zshrc $HOME/.zshrc
-ln -s $DOTFILES/.zshenv $HOME/.zshenv
-ln -s $DOTFILES/.zprofile $HOME/.zprofile
-ln -s $DOTFILES/.vim $HOME/.vim
-ln -s $DOTFILES/.vimrc $HOME/.vimrc
-ln -s $DOTFILES/.tmux.conf $HOME/.tmux.conf
+ln -s $DOTFILES/.zshrc      $HOME/.zshrc
+ln -s $DOTFILES/.zshenv     $HOME/.zshenv
+ln -s $DOTFILES/.zprofile   $HOME/.zprofile
+ln -s $DOTFILES/.vim/       $HOME/.vim
+ln -s $DOTFILES/.vimrc      $HOME/.vimrc
+ln -s $DOTFILES/.tmux.conf  $HOME/.tmux.conf
 
-vim -c ':NeoBundleInstall'
+vim -c ':NeoBundleInstall' -c q
 
 ZSH=`which zsh`
-
-echo ZSH
-echo ZSH
-echo ZSH
+chsh -s $ZSH
 
