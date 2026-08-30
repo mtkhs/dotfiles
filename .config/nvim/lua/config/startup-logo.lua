34af1114b64043d7835606d139d317b8882a2983
Motoi TAKAHASHI <openmoyo@gmail.com>
2026-08-31 00:05:46 +0900

    Remove unused augroup helper from startup-logo

--- .config/nvim/lua/config/startup-logo.lua
+++ .config/nvim/lua/config/startup-logo.lua
@@ -8,16 +8,6 @@
 -- augroup for this config file
 local augroup = vim.api.nvim_create_augroup('startup-logo', {})
 
--- wrapper function to use internal augroup
-local function create_autocmd(event, opts)
-  vim.api.nvim_create_autocmd(
-    event,
-    vim.tbl_extend('force', {
-      group = augroup,
-    }, opts)
-  )
-end
-
 local logo = [[
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
